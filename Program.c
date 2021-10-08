f868d6a8f0782138233146533e2aa220a3187171
Aman Kumar <amanofficial16@gmail.com>
2021-10-08 11:25:52 +0530

    Debugged the code

--- Program.c
+++ Program.c
@@ -2,17 +2,20 @@
 
 int main()
 {
-    char name,country,skill;
+    char name[100];
+    char country[100];
+    char skill;
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    gets(name);
     printf("Enter your Country of Residance : ");//Get user country
-    scanf("%s",&country);
+    gets(country);
     printf("Enter your Age : ");//Get user Age
-    printf("%f",&age);
-    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
-    scanf(" %c",skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    scanf("%d",&age);
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest) : ");//Get user skill level
+    scanf(" %c",&skill);
+    printf("Thank you %s Happy Hacking",name);//Thank the user
 
     return 0;
 }
